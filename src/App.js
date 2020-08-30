9a41c4ecabdbb09bbc3c39ffaf1c18059113a627
Farhan Alam <farhan_w901@outlook.com>
2020-08-30 23:46:10 +0600

    added legend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import json from './data/data.json'
 
 export default function App() {
 
-	const margin = { left: 50, right: 20, top: 10, bottom: 40 };
+	const margin = { left: 50, right: 50, top: 10, bottom: 40 };
 	const width = 600 - margin.left - margin.right,
 		height = 400 - margin.top - margin.bottom;
 	const timer = 800;
@@ -68,7 +68,8 @@ export default function App() {
 
 		var yLabel = g.append('text')
 			.attr('x', -(height / 2))
-			.attr('y', -(margin.left - margin.right))
+			// .attr('y', -(margin.left - margin.right))
+			.attr('y', -30)
 			.attr('text-anchor', 'middle')
 			.attr('font-size', '18px')
 			.attr('text-anchor', 'middle')
@@ -81,6 +82,8 @@ export default function App() {
 			.attr('class', 'year-label');
 
 		var t = d3.transition().duration(timer / 3);
+
+		addLegend(g);
 
 		var update = () => {
 			let data = json[index].countries;
@@ -121,6 +124,27 @@ export default function App() {
 			update();
 		}, timer);
 		update();
+	}
+
+	function addLegend(g) {
+		var legendGroup = g.append('g');
+		
+		['Asia', 'Africa', 'Europe', 'Americas'].map((v, i) => {
+			let y = i * 40
+			legendGroup.append('rect')
+				.attr('width', 10)
+				.attr('height', 10)
+				.attr('x', (width + (margin.right / 2)) - 5)
+				.attr('class', `point fill-${v.toLowerCase()}`)
+				.attr('y', y)
+			legendGroup.append('text')
+				.attr('x', (width + (margin.right / 2)))
+				.attr('y', y + 25)
+				.attr('class', 'legend')
+				.text(v)
+		});
+
+		legendGroup.attr('transform', `translate(0, 70)`)
 	}
 
 
