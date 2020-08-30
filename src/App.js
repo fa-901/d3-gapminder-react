537778976ea82c01bd4f688c7811550fd17ef73a
Farhan Alam <farhan_w901@outlook.com>
2020-08-30 15:15:27 +0600

    us d3 from npm package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useEffect } from "react";
+import * as d3 from 'd3';
 import "./styles.css";
 import data from './data/data.json'
 
 export default function App() {
+
+	const margin = { left: 70, right: 20, top: 10, bottom: 35 };
+	const width = 600 - margin.left - margin.right,
+		height = 400 - margin.top - margin.bottom;
 
 	useEffect(()=>{
 		startChart();
 	}, []);
 
 	function startChart() {
-		console.log(data)
+		console.log(data);
+
+		var g = d3.select("#chart-area")
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", `translate(${margin.left}, ${margin.top})`);
 	}
 
 
